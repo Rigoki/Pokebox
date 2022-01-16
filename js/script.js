a6b4a712b4f45502dd3d68e79988ba92ebb35fcd
Rigoki <97304331+Rigoki@users.noreply.github.com>
2022-01-16 19:44:02 +0100

    Add Filter

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+$(function(){
+$('#feu').on({
+    click: ()=> {
+        $("#galerie img:not('.feu')").fadeTo(100,0.4);
+        $(".feu").fadeTo(100,1)
+    }
+})
+$('#plante').on({
+    click: ()=> {
+        $("#galerie img:not('.plante')").fadeTo(100,0.4)
+        $(".plante").fadeTo(100,1)
+    }
+})
+$('#eau').on({
+    click: ()=> {
+        $("#galerie img:not('.eau')").fadeTo(100,0.4)
+        $(".eau").fadeTo(100,1)
+    }
+})
+$('#reset').on({
+    click: ()=> {
+        $("#galerie img").fadeTo(100,1)
+    }
+})
+
+})
