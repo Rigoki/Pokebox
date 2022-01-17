197180a4a7e65630a70f6b3ed2fe18848ad7766a
Rigoki <97304331+Rigoki@users.noreply.github.com>
2022-01-17 08:44:40 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,50 +1,57 @@
 /*JS*/
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-/*JQ*/
-$(function(){
-$("#troll").hide()
-$('#feu').on({
-    click: ()=> {
-        $("#galerie img:not('.feu')").fadeTo(100,0.4);
-        $(".feu").fadeTo(100,1)
-    }
-})
-$('#plante').on({
-    click: ()=> {
-        $("#galerie img:not('.plante')").fadeTo(100,0.4)
-        $(".plante").fadeTo(100,1)
-    }
-})
-$('#eau').on({
-    click: ()=> {
-        $("#galerie img:not('.eau')").fadeTo(100,0.4)
-        $(".eau").fadeTo(100,1)
-    }
-})
-$('#reset').on({
-    click: ()=> {
-        $("#galerie img").fadeTo(100,1)
-    }
-})
-$("#galerie").on({
-click : (e) =>{
-    if (!e.target.src) {} else {
-        let name = e.target.name;
-        $("#cardTxt").hide().html("<h2> " + name + "</h2>").slideDown(400);
-        let img = e.target.src;
-        $("#cardImg").hide().html( "<img src=" + img + " class='rounded'>").fadeIn(900);
-        let com = e.target.alt;
-        $("#cardCom").hide().html("<p>" + com + "</p>").slideDown(1000);
-        $("#troll").show()
-    }
-}
-})
-$("#troll").on({
-    click: () =>{
-        $("#audio")[0].play()
-    }
-})
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    /*JQ*/
+$(function() {
+    $("#troll").hide()
+    $('#feu').on({
+        click: () => {
+            $("#galerie img:not('.feu')").fadeTo(100, 0.4);
+            $(".feu").fadeTo(100, 1)
+        }
+    })
+    $('#plante').on({
+        click: () => {
+            $("#galerie img:not('.plante')").fadeTo(100, 0.4)
+            $(".plante").fadeTo(100, 1)
+        }
+    })
+    $('#eau').on({
+        click: () => {
+            $("#galerie img:not('.eau')").fadeTo(100, 0.4)
+            $(".eau").fadeTo(100, 1)
+        }
+    })
+    $('#reset').on({
+        click: () => {
+            $("#galerie img").fadeTo(100, 1)
+        }
+    })
+    $("#galerie").on({
+        click: (e) => {
+            if (!e.target.src) {} else {
+                $("#troll").hide().delay(1000).fadeIn(1000);
+                let name = e.target.name;
+                $("#cardTxt").hide().html("<h2> " + name + "</h2>").slideDown(400);
+                let img = e.target.src;
+                $("#cardImg").hide().delay(300).html("<img src=" + img + " class='rounded'>").fadeIn(900);
+                let com = e.target.alt;
+                $("#cardCom").hide().delay(600).html("<p>" + com + "</p>").fadeIn(1000)
+            }
+        }
+    })
+    $("#troll").on({
+        click: () => {
+            $("#audio")[0].play();
+            $("#troll img").animate({
+                width: "-=2em",
+                height: "-=2em",
+            }).animate({
+                width: "+=2em",
+                height: "+=2em"
+            })
+        }
+    })
 })
